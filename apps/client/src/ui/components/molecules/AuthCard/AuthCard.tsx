313e3c3ec0f2220cdaef034ea67781dd4e263b09
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-04-26 18:42:24 +0330

    refactor(client): migrate AuthCard styles to tailwindcss

--- apps/client/src/ui/components/molecules/AuthCard/AuthCard.tsx
+++ apps/client/src/ui/components/molecules/AuthCard/AuthCard.tsx
@@ -1,15 +1,11 @@
-import $ from "./AuthCard.module.scss";
-
 type Props = {
   children: React.ReactNode;
 };
 
 const AuthCard = ({ children }: Props) => {
   return (
-    <section className={$.auth_card}>
-      <div className={$.content}>
-        {children}
-      </div>
+    <section className="bg-background-secondary h-[60rem] rounded-lg shadow-custom text-center p-8">
+      <div className="h-auto pt-[36%]">{children}</div>
     </section>
   );
 };
